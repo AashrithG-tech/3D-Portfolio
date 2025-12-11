cbd44d6b7565d042c61585952c62868cb7f87402
AASHRITH <aashrith@Mac.lan>
2025-12-11 11:16:49 +0530

    Implemented Showcase section

--- src/components/Animatedcounter.jsx
+++ src/components/Animatedcounter.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import {counterItems} from "../constants/index.js";
 import CountUp from "react-countup";
 
+
 const Animatedcounter = () => {
     return (
-        <div className="padding-x-lg xl:mt-0 mt-32" id="counter">
-            <div className="mx-auto grid-cols-4">
+        <div className="padding-x-lg xl:mt-0 mt-32 flex flex-row" id="counter">
+            <div className="mx-auto grid grid-cols-4 gap-10px">
                 {counterItems.map((item) => (
                     <div className="bg-zinc-900 flex flex-col rounded-lg p-10 justify-center">
                         <div key={counterItems.label} className="counter-number text-white-500 text-5xl font-bold mb-2">
